d45fbb6c6972b45da0ad15ac91e640376f5fc071
Alex <ax.dev@yahoo.com>
2023-05-18 03:19:18 +0300

    Anti-aliasing

--- app_creator.py
+++ app_creator.py
@@ -1,27 +1,24 @@
-from PIL import Image, ImageDraw
+import cv2
+import numpy as np
 import random
 import math
 
 def generate_texture(min_triangle_size, max_triangle_size, resolution, background_color, start_color, end_color):
-    image = Image.new('RGB', resolution, background_color)
-    draw = ImageDraw.Draw(image)
+    image = np.zeros((resolution[1], resolution[0], 3), dtype=np.uint8)
+    image[:] = background_color
     
     for _ in range(num_triangles):
         triangle_size = random.randint(min_triangle_size, max_triangle_size)
         triangle_color = generate_gradient_color(start_color, end_color)
         
-        x1, y1, x2, y2, x3, y3 = generate_triangle_coordinates(resolution, triangle_size)
-        vertices = [(x1, y1), (x2, y2), (x3, y3)]
+        x, y, w, h, angle = generate_triangle_properties(resolution, triangle_size)
         
-        center_x = (x1 + x2 + x3) // 3
-        center_y = (y1 + y2 + y3) // 3
+        # Создание треугольника
+        triangle = np.array([(x, y), (x + w, y), (x, y + h)], np.int32)
+        rotated_triangle = rotate_triangle(triangle, angle, (x + w/2, y + h/2))
         
-        # Вращение треугольника
-        angle = random.uniform(0, 2*math.pi)
-        rotated_vertices = rotate_triangle(vertices, angle, (center_x, center_y))
-    
-        # Рисование треугольника
-        draw.polygon(rotated_vertices, triangle_color)
+        # Заполнение треугольника сглаженным цветом
+        cv2.fillPoly(image, [rotated_triangle], triangle_color)
     
     return image
 
@@ -33,40 +30,43 @@ def generate_gradient_color(start_color, end_color):
     colors.append(end_color)
     return random.choice(colors)
 
-def generate_triangle_coordinates(resolution, triangle_size):
+def generate_triangle_properties(resolution, triangle_size):
     x_range = resolution[0] - triangle_size
     y_range = resolution[1] - triangle_size
     
-    x1 = random.randint(0, x_range)
-    y1 = random.randint(0, y_range)
+    x = random.randint(0, x_range)
+    y = random.randint(0, y_range)
     
-    x2 = x1 + triangle_size
-    y2 = y1
+    w = random.randint(0, triangle_size)
+    h = random.randint(0, triangle_size)
     
-    x3 = random.randint(x1, x2)
-    y3 = y1 + triangle_size
+    angle = random.uniform(0, 2*math.pi)
     
-    return x1, y1, x2, y2, x3, y3
+    return x, y, w, h, angle
 
-def rotate_triangle(vertices, angle, center):
-    rotated_vertices = []
-    for x, y in vertices:
+def rotate_triangle(triangle, angle, center):
+    rotated_triangle = []
+    for x, y in triangle:
         x -= center[0]
         y -= center[1]
         rotated_x = x * math.cos(angle) - y * math.sin(angle)
         rotated_y = x * math.sin(angle) + y * math.cos(angle)
         rotated_x += center[0]
         rotated_y += center[1]
-        rotated_vertices.append((rotated_x, rotated_y))
-    return rotated_vertices
+        rotated_triangle.append((rotated_x, rotated_y))
+    return np.array(rotated_triangle, np.int32)
 
-min_triangle_size = 50
-max_triangle_size = 150
+# Параметры текстуры
+min_triangle_size = 120
+max_triangle_size = 180
 resolution = (1920, 1080)
 background_color = (255, 255, 255)
-start_color = (255, 10, 10)
-end_color = (10, 10, 255)
-num_triangles = random.randint(80, 160)
+start_color = (255, 0, 0)
+end_color = (0, 0, 255)
+num_triangles = random.randint(80, 150)
 
+# Создание текстуры
 texture = generate_texture(min_triangle_size, max_triangle_size, resolution, background_color, start_color, end_color)
-texture.save('generated_texture.png')
+
+# Сохранение текстуры в файл
+cv2.imwrite('generated_texture.png', texture)
